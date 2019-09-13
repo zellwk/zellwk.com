55ebe7d6d52a462c0d2b0291622f8153948b69d1
Zell Liew <zellwk@gmail.com>
2019-09-13 09:05:22 +0800

    update gilupfile
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- gulp/images.js
+++ gulp/images.js
@@ -1,31 +1,12 @@
 const { src, dest, series, parallel } = require('gulp')
 const imagemin = require('gulp-imagemin')
-const webp = require('gulp-webp')
 const newer = require('gulp-newer')
-const resizer = require('@zellwk/resize-images')
 
-const { input, output, imageSizes } = require('./_config')
+const { input, output } = require('./_config')
 
 const imageInput = `${input}/images/**/*`
 const tmpOutput = `./_tmp/minified`
-const tmpOutput2 = `./_tmp/resized`
 const imageOutput = `${output}/images`
-
-const jpegToWebp = _ => {
-  return src(imageInput + `.{jpg,jpeg}`)
-    .pipe(newer({ dest: tmpOutput, ext: '.webp' }))
-    // Lower quality because webp size can grow larger than its source
-    // Reasons: https://developers.google.com/speed/webp/faq
-    .pipe(webp({ quality: 80 }))
-    .pipe(dest(tmpOutput))
-}
-
-const pngToWebp = _ => {
-  return src(imageInput + `.png`)
-    .pipe(newer({ dest: tmpOutput, ext: '.webp' }))
-    .pipe(webp({ lossless: true }))
-    .pipe(dest(tmpOutput))
-}
 
 const minifyImages = _ => {
   return src(imageInput + `.{png,jpg,jpeg,gif}`)
@@ -37,16 +18,8 @@ const minifyImages = _ => {
     .pipe(dest(tmpOutput))
 }
 
-// const resizeImages = _ => {
-//   return resizer({
-//     inputDir: tmpOutput,
-//     outputDir: tmpOutput2,
-//     outputSizes: imageSizes
-//   })
-// }
-
 const copyImagesToDist = _ => {
-  return src(tmpOutput2 + `/**/*`)
+  return src(tmpOutput + `/**/*`)
     .pipe(dest(imageOutput))
 }
 
@@ -61,8 +34,7 @@ const copyFaviconsToDist = _ => {
 }
 
 const images = series(
-  parallel(jpegToWebp, pngToWebp, minifyImages),
-  // parallel(resizeImages),
+  parallel(minifyImages),
   parallel(copyImagesToDist, copySvgToDist, copyFaviconsToDist)
 )
 
