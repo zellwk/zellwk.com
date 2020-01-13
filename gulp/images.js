19d50e0e044a9b117e06e6a9f39d28e01b50ccd9
Zell Liew <zellwk@gmail.com>
2020-01-13 11:49:34 +0800

    Add Learn JavaScript design part 2

--- gulp/images.js
+++ gulp/images.js
@@ -5,11 +5,11 @@ const newer = require('gulp-newer')
 const { input, output } = require('./_config')
 
 const imageInput = `${input}/images/**/*`
-const tmpOutput = `./_tmp/minified`
+const tmpOutput = './_tmp/minified'
 const imageOutput = `${output}/images`
 
 const minifyImages = _ => {
-  return src(imageInput + `.{png,jpg,jpeg,gif}`)
+  return src(imageInput + '.{png,jpg,jpeg,gif}')
     .pipe(newer(tmpOutput))
     .pipe(imagemin([
       imagemin.jpegtran({ progressive: true }),
@@ -19,23 +19,28 @@ const minifyImages = _ => {
 }
 
 const copyImagesToDist = _ => {
-  return src(tmpOutput + `/**/*`)
+  return src(tmpOutput + '/**/*')
     .pipe(dest(imageOutput))
 }
 
 const copySvgToDist = _ => {
-  return src(imageInput + `.svg`)
+  return src(imageInput + '.svg')
     .pipe(dest(imageOutput))
 }
 
 const copyFaviconsToDist = _ => {
-  return src(input + `/favicons/**/*`)
+  return src(input + '/favicons/**/*')
     .pipe(dest(imageOutput + '/favicons'))
+}
+
+const copyVideosToDist = _ => {
+  return src(imageInput + '.mp4')
+    .pipe(dest(imageOutput))
 }
 
 const images = series(
   parallel(minifyImages),
-  parallel(copyImagesToDist, copySvgToDist, copyFaviconsToDist)
+  parallel(copyImagesToDist, copySvgToDist, copyFaviconsToDist, copyVideosToDist)
 )
 
 exports.default = images
