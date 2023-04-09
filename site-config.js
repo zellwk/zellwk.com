6cbb71a8b99f87631ab68b8df1ed4279dee4c50b
Zell Liew <zellwk@gmail.com>
2023-04-10 01:40:29 +0800

    Fix sync dir

--- site-config.js
+++ site-config.js
@@ -5,7 +5,7 @@ export default {
   siteUrl: 'https://zellwk.com',
 
   port: getPort(),
-  syncDestination: '/var/www/zellwk.com/dist',
+  syncDestination: '/var/www/zellwk.com',
 }
 
 function getPort(mode) {
