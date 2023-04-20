77763303a99c0f319948e0c43ad7aba37f5520af
Zell Liew <zellwk@gmail.com>
2023-04-20 14:21:26 +0800

    Update Lead Modal Implementation

--- src/components/LeadModal.svelte
+++ src/components/LeadModal.svelte
@@ -9,7 +9,7 @@
     getCkIDFromQueryParams,
     saveCkID,
     isSubscriber,
-  } from './ConvertkitForm'
+  } from './Convertkit.js'
 
   let modal = {
     state: 'closed',
@@ -21,11 +21,8 @@
     if (id) saveCkID()
   })
 
-  // ========================
-  // Event Listeners
-  // ========================
   function openModal() {
-    if (!shouldOpen()) return
+    if (isSubscriber()) return false
 
     modal.launcherRect = {
       top: document.documentElement.clientHeight / 2,
@@ -37,33 +34,39 @@
     modal.state = 'open'
   }
 
-  function shouldOpen() {
-    const ckSubscriber = isSubscriber()
-    if (ckSubscriber) return false
+  // ========================
+  // Event Listeners
+  // ========================
+  // Check whether the user has already seen the modal on scroll.
+  // If displayed before, check if the expiry is still valid.
+  // If expiry timestamp is still valid, don't show the modal
+  function scrollCheck() {
+    // LMSE = Lead Modal on Scroll Expiry Timestamp
+    const expiry = localStore.get('LMSE')
+    if (expiry > Date.now()) return false
 
-    // Check whether the user has already seen the modal
-    // If LeadModalDisplayed is true, check if LeadModalExpiry is still valid
-    // If LeadModalExpiry is still valid, don't show the modal
-    const LeadModalDisplayed = localStore.get('LeadModalDisplayed')
-    const LeadModalExpiry = localStore.get('LeadModalExpiryTimestamp')
-    if (LeadModalDisplayed && LeadModalExpiry > Date.now()) return false
+    localStore.set('LMSE', Date.now() + milliseconds({ days: 7 }))
+    openModal()
+  }
 
-    // Show the Modal and set new values
-    localStore.set('LeadModalDisplayed', true)
-    localStore.set(
-      'LeadModalExpiryTimestamp',
-      Date.now() + milliseconds({ days: 7 })
-    )
+  // Check whether the user has already seen the modal on exit intent.
+  // If displayed before, check if the expiry is still valid.
+  // If expiry timestamp is still valid, don't show the modal
+  function exitCheck() {
+    // LMEE = Lead Modal on Exit Expiry Timestamp
+    const expiry = localStore.get('LMEE')
+    if (expiry > Date.now()) return false
 
-    return true
+    localStore.set('LMEE', Date.now() + milliseconds({ days: 7 }))
+    openModal()
   }
 </script>
 
 <svelte:document
   use:ScrollObserver={{ threshold: 0.5 }}
-  on:down:threshold={openModal}
+  on:down:threshold={scrollCheck}
   use:ExitIntent
-  on:exit={openModal}
+  on:exit={exitCheck}
 />
 
 {#if modal.state === 'open'}
