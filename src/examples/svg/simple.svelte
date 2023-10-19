7752c7f95c9c1bca06ed25c9834b69dd15de0c9c
Zell Liew <zellwk@gmail.com>
2023-10-19 17:06:00 +0800

    Add articles

---
+++ src/examples/svg/simple.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Callout from '../../components/Callout.svelte'
+  import { SVG } from 'splendid-ui/svelte'
+</script>
+
+<Callout type="demo">
+  <div class="Box">
+    <SVG icon="tabler:menu" /> Tabler's Menu
+  </div>
+
+  <div class="Box">
+    <SVG icon="material-symbols:menu" /> Material Symbol's menu
+  </div>
+
+  <div class="Box">
+    <SVG icon="zondicons:menu" /> Zondicon's menu
+  </div>
+</Callout>
+
+<style>
+  .Box {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+</style>
