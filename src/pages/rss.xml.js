1bac5ed22d8f05cff4f7b5ceed655c0e4d7706a1
Zell Liew <zellwk@gmail.com>
2025-11-17 22:51:54 +0800

    Fix RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,34 +1,37 @@
-import { getContainerRenderer as getMDXRenderer } from '@astrojs/mdx'
-import { getContainerRenderer as svelteContainerRenderer } from '@astrojs/svelte'
+import mdxRenderer from '@astrojs/mdx/server.js'
+import svelteRenderer from '@astrojs/svelte/server.js'
 import { components } from '../components/blogPostComponents.js'
 
 import rss from '@astrojs/rss'
 import { processFiles } from '@splendidlabz/astro/content'
 import { toSentence } from '@splendidlabz/utils'
 import { experimental_AstroContainer as AstroContainer } from 'astro/container'
-import { loadRenderers } from 'astro:container'
 import { getCollection, render } from 'astro:content'
 import config from '../../site-config.js'
 
+export const prerender = true
+
 export async function GET(context) {
-  const renderers = await loadRenderers([
-    svelteContainerRenderer(),
-    getMDXRenderer(),
-  ])
-  const container = await AstroContainer.create({ renderers })
+  const container = await AstroContainer.create()
+
+  container.addServerRenderer({ renderer: svelteRenderer })
+  container.addServerRenderer({ renderer: mdxRenderer })
 
   const blog = await getCollection('blog')
   const posts = await await processFiles(blog)
   const items = posts.map(async (post, index) => {
-    const { Content } = await render(post)
+    const rendered = await render(post)
+    const { Content } = rendered
+    const content = await container.renderToString(Content, {
+      props: { components },
+    })
+
     const data = {
-      link: new URL(`/blog/${post.slug}`, context.url.origin).toString(),
+      link: `${config.site.url}/blog/${post.data.slug}/`,
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
-      content: await container.renderToString(Content, {
-        props: { components },
-      }),
+      content,
       customData: post.data.tags
         .map(category => `<category>${toSentence(category)}</category>`)
         .join(''),
