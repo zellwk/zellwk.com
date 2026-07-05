239aa566dd1d81b495fea2d0a3ba3b4c6223b4f6
Zell Liew <zellwk@gmail.com>
2026-07-05 22:24:59 +0800

    updates

---
+++ src/content/data/faqs/unorthodox-tailwind.js
@@ -0,0 +1,28 @@
+export const faq = [
+  {
+    question: 'What version of Tailwind does this use?',
+    answer: `This system here use v4. It works exceptionally well because v4 made it super easy for anyone to create utilities.
+    
+    If you're still on v3, the mindset shifts carry over, but the process to making utilities and custom variants is a bit different.`,
+  },
+  {
+    question: 'What happens when Tailwind changes again?',
+    answer: `Everything we cover here is built on Tailwind and CSS fundamentals that are unlikely to change.`,
+  },
+  {
+    question: `Will this be right for my level?`,
+    answer: `Yes. The Unorthodox Fundamentals are new to everyone --- because there's simply nothing like that out there. 
+    
+    If you're super new to Tailwind, the Tailwind Fundamentals chapter is there to get you up to speed really quickly.
+    
+    So this course is designed for both beginners and advanced developers.`,
+  },
+  {
+    question: 'Is this a video course?',
+    answer: `No. This is a written course with code you can copy. I wrote this in text because I want this to be a reference that you can come back to, because text is easy to find, search, and you can even get your AI to help 😉.`,
+  },
+  {
+    question: `What if it's not for me?`,
+    answer: `Just let me know within 30 days and I'll send you a refund.`,
+  },
+]
